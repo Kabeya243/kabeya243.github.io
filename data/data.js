0a1aa41bf7d7caf1da27ac15a58e71e1bf849ef0
Kabeya243 <kabeyailunga243@gmail.com>
2023-05-28 16:49:55 +0200

    modification de works

---
+++ data/data.js
@@ -0,0 +1,26 @@
+export const works = [
+    {
+        title:"Cafeo's love",
+        img:"./img/cafeo.png",
+        desc:`site web Brutal pure avec un design unique et captivant pour une entreprise de cafe made in africa!`,
+        link:"#",
+    },
+    {
+        title:"elixir wine",
+        "img":"./img/elixir.png",
+        desc:`site web moderne & pure avec un design unique et captivant pour une entreprise des vins 100% africain!`,
+        link:"#",
+    },
+    {
+        title:"Iterior design",
+        "img":"./img/newslife.png",
+        desc:`site web Minimaliste epuree avec un design propre & unique et captivant pour une entreprise de design interieur`,
+        link:"#",
+    },
+    {
+        title:"Cafeo's love",
+        "img":"./img/porfolio1.png",
+        desc:`site web Brutal pure avec un design unique et captivant pour une entreprise de cafe made in africa!`,
+        link:"#",
+    },
+]
